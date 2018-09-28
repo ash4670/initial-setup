4de6c1b4aca1dc166883f3f1f0b0c15a17ea87ae
ashraf <ash4670@yahoo.com>
2018-09-28 18:36:12 +0200

    after roter module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,15 +7,33 @@ import { FormsModule } from '@angular/forms';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import { MatListModule } from '@angular/material/list';
 import {FlexLayoutModule} from '@angular/flex-layout' ;
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+
 import 'hammerjs' ;
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { DishService } from './services/dish.service';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { DishdetailComponent } from './dishdetail/dishdetail.component';
 
+import { AppRoutingModule } from './app-routing/app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    HeaderComponent,
+    FooterComponent,
+    AboutComponent,
+    HomeComponent,
+    ContactComponent,
+    DishdetailComponent
   ],
   imports: [
     BrowserModule,
@@ -23,9 +41,10 @@ import { MenuComponent } from './menu/menu.component';
     FormsModule,
     HttpModule ,
     MatToolbarModule,
-    FlexLayoutModule,MatListModule
+    FlexLayoutModule,MatListModule,MatGridListModule,MatCardModule,MatButtonModule
+    ,AppRoutingModule
   ],
-  providers: [],
+  providers: [DishService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
